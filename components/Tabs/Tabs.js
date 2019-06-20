03ff070b886c8e1b5525cc39fe31da02cb26d2cd
noahfranco <noahfra@gmail.com>
2019-06-20 17:18:52 -0400

    finished class section of Tabs.js file

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -11,18 +11,20 @@ class TabLink {
     this.itemElement = this.element.querySelector(`.tabs-item[data-tabs=${this.data}]`); 
     
     // Using the Item element, create a new instance of the TabItem class
-    this.tabItem = this.element. 
+    this.tabItem = new TabItem(this.tabItem); 
     
     // Add a click event listener on this instance, calling the select method on click
-
+      this.tabItem.addEventListener('click', event => {
+        this.select()
+      })
   };
 
   select() {
     // Get all of the elements with the tabs-link class
-    // const links;
+    const links = document.querySelectorAll(".tab-links")
 
     // Using a loop or the forEach method remove the 'tabs-link-selected' class from all of the links
-    // Array.from(links).forEach();
+    Array.from(links).forEach();
 
     // Add a class named "tabs-link-selected" to this link
     // this.element;
@@ -59,4 +61,4 @@ class TabItem {
 
 */
 
-links = document.querySelectorAll();
+// links = document.querySelectorAll();
