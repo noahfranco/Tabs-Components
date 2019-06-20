81b4870855d485435b67f9cbbfcb149ea192a33f
noahfranco <noahfra@gmail.com>
2019-06-20 16:51:24 -0400

    finished dropdown.js file

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -2,16 +2,16 @@
 class TabLink {
   constructor(element) {
     // Assign this.element to the passed in DOM element
-    // this.element;
+    this.element = element; 
     
     // Get the custom data attribute on the Link
-    // this.data;
+    this.data = element.dataset.tab; 
     
     // Using the custom data attribute get the associated Item element
-    // this.itemElement;
+    this.itemElement = this.element.querySelector(`.tabs-item[data-tabs=${this.data}]`); 
     
     // Using the Item element, create a new instance of the TabItem class
-    // this.tabItem;
+    this.tabItem = this.element. 
     
     // Add a click event listener on this instance, calling the select method on click
 
